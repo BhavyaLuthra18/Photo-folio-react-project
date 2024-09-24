b65650f0f8e1f294403f87d3f21d705d569bb76e
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2024-09-24 15:21:05 +0530

    Fixes and ui betterment

--- src/components/images/Image.js
+++ src/components/images/Image.js
@@ -1,34 +1,46 @@
-// import styles 
-import styles from "./image.module.css"
+// import styles
+import styles from "./image.module.css";
 
 // to show all the images within an album
-export default function Image({image,index,handleImageEdit,handleImageDelete,openLightbox}){
-    return(
-        <>
-        {/* main container of Image */}
-        <div className={styles.imageCard}>
-         {/* showing image */}
-         <div className={styles.imageBox}>
-         <img src={image.link}
-         alt ="image"
-        onClick={() =>openLightbox(index)}
-        />
+export default function Image({
+  image,
+  index,
+  handleImageEdit,
+  handleImageDelete,
+  openLightbox,
+}) {
+  return (
+    <>
+      {/* main container of Image */}
+      <div className={styles.imageCard}>
+        {/* showing image */}
+        <div className={styles.imageBox}>
+          <img
+            src={image.link}
+            alt="image"
+            onClick={() => openLightbox(index)}
+          />
         </div>
-        
-      {/* Image name with buttons to delete or edit image */}
-       <div className={styles.imageInfo}>
-       {image.name}
-            {/* for edit */}
-            <button className={`${styles.imageBtn} ${styles.editBtn}`} 
-                        onClick={()=>handleImageEdit(image)}>Edit
+
+        {/* Image name with buttons to delete or edit image */}
+        <div className={styles.imageInfo}>
+          {image.name}
+          {/* for edit */}
+          <button
+            className={`${styles.imageBtn} ${styles.editBtn}`}
+            onClick={() => handleImageEdit(image)}
+          >
+            Edit
           </button>
           {/* for delete */}
-          <button className={`${styles.imageBtn} ${styles.deleteBtn}`} 
-                        onClick={()=>handleImageDelete(image)}>X
-         </button>
-        
-        </div> 
-     </div>
-   </>
- )
+          <button
+            className={`${styles.imageBtn} ${styles.deleteBtn}`}
+            onClick={() => handleImageDelete(image)}
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
